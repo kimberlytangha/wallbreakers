23001ff1126cc8af2ebf25394b3de73d69760dcb
Kimberly Ha <kimberlytangha@gmail.com>
2019-07-13 14:30:34 -0400

    Redid.

--- week1/detect_capital.cpp
+++ week1/detect_capital.cpp
@@ -3,8 +3,8 @@ public:
     // time O(n)
     // space O(1)
     bool detectCapitalUse(string s) {
-        bool firstUpper = isupper(s[0]) ? 1 : 0; 
-        bool secondUpper = isupper(s[1]) ? 1 : 0; 
+        bool firstUpper = isupper(s[0]); 
+        bool secondUpper = isupper(s[1]);
         
         for (int i = 1; i < s.length(); i++) {
             if (firstUpper) {
@@ -14,6 +14,33 @@ public:
                 return false; 
         }
 
+        return true; 
+    }
+};
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = word.length(); 
+        if (n == 1) 
+            return true; 
+        
+        bool upperFirst = isupper(word[0]);
+        bool upperSecond = isupper(word[1]);
+        
+        if (!upperFirst && upperSecond)
+            return false; 
+        
+        for (int i = 2; i < n; i++) {
+            if (upperFirst && upperSecond) {
+                if (!isupper(word[i])) 
+                    return false; 
+            } else {
+                if (isupper(word[i]))
+                    return false; 
+            }
+        }
+        
         return true; 
     }
 };
