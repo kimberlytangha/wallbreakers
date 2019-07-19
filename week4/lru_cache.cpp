4237be57c2d8e5fa9dfaa6fe88579a5880c79d72
Kimberly Ha <kimberlytangha@gmail.com>
2019-07-19 14:47:59 -0400

    added comments.

--- week4/lru_cache.cpp
+++ week4/lru_cache.cpp
@@ -1,70 +1,8 @@
-// class LRUCache {
-// public:
-//     unordered_map<int,int> cache;   
-//     list<int> mostUsed; 
-//     int maxCount; 
-    
-//     LRUCache(int capacity) {
-//         maxCount = capacity; 
-//     }
-    
-//     int get(int key) {
-//         if (cache.find(key) != cache.end()) {
-//             // update mostUsed
-//             if (mostUsed.front() != key) {
-//                 mostUsed.remove(key);
-//                 mostUsed.push_front(key);     
-//             }
-            
-//             return cache[key];
-//         } else {
-//             return -1; 
-//         }
-//     }
-    
-//     void put(int key, int value) {
-//         if (cache.size() < maxCount) {
-//             if (cache.find(key) == cache.end()) {
-//                 // update most used
-//                 mostUsed.push_front(key);     
-//             }
-            
-//             cache[key] = value;
-//         } else {
-//             if (cache.find(key) != cache.end()) {
-//                 // if there is a match in the cache
-
-//                 // update mostUsed
-//                 mostUsed.remove(key);
-//                 mostUsed.push_front(key);
-
-//                 // update cache
-//                 cache[key] = value;
-//             } else {
-//                 // else there is not a match in the cache
-                
-//                 // grab least used key and store in var
-//                 int toRemove = mostUsed.back(); 
-
-//                 // remove key from both memory stores
-//                 mostUsed.remove(toRemove);
-//                 cache.erase(toRemove);
-
-//                 // add new entries 
-//                 mostUsed.push_front(key);
-//                 cache[key] = value;     
-//             }
-            
-//         }
-//     }
-// };
-
 class LRUCache {
 public:
     int capacity; 
-    
-    // declares iterator to list 
-    // list<pair<int,int>>::iterator 
+
+    // map : <key, reference to ListNode in dll> 
     unordered_map<int, list<pair<int,int>>::iterator> hm; 
     
     // holds most recently used key, value pairs
@@ -81,39 +19,36 @@ public:
             // key not found
             return -1;
         
-        // auto tmpIter = recent.find(iter);
-        // int val = tmpNode.second; 
-        // recent.erase(iter); 
-        // recent.emplace_front(key, val); 
+        list<pair<int,int>>::iterator listNode = iter->second;
+        // inserts into front of dll 
+        recent.splice(recent.begin(), recent, listNode);
         
-        // updates recents to reflect usage 
-        recent.splice(recent.begin(), recent, iter->second);
-        
-        return iter->second->second; 
+        return listNode->second; 
     }
     
     void put(int key, int value) {
         auto iter = hm.find(key); 
         
         if (iter != hm.end()) { // key exists
+            list<pair<int,int>>::iterator listNode = iter->second;
+            
             // move node to front of recent 
-            recent.splice(recent.begin(), recent, iter->second);
+            recent.splice(recent.begin(), recent, listNode);
             
             // update value 
-            iter->second->second = value; 
+            listNode->second = value; 
             return; 
         }
         
+        // at capacity, delete least recently used 
         if (hm.size() == capacity) {
-            // remove least used node from recents and map 
             int deleteKey = recent.back().first; 
             recent.pop_back();
             hm.erase(deleteKey);
         }
         
-        // place new node at front of recents 
+        // add to front of dll 
         recent.emplace_front(key, value);
-        // add new entry into map 
         hm[key] = recent.begin();  
     }
 };
